767b4c95cb36035dfa04d770acaa17a45f4e5dd3
Maxime Leguevacques <m.leguevacques@student.isartdigital.com>
2024-09-16 16:26:57 +0200

    Initial commit.

---
+++ Source/LeDesinforme/LeDesinforme.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "LeDesinforme.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LeDesinforme, "LeDesinforme" );
