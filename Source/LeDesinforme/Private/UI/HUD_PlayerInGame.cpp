a89f8e94d9121b10790c2713db2afe7b98b0dd19
Maxime Leguevacques <m.leguevacques@student.isartdigital.com>
2024-09-18 12:02:36 +0200

    Added UI assets and working on UI.

--- Source/LeDesinforme/Private/UI/HUD_PlayerInGame.cpp
+++ Source/LeDesinforme/Private/UI/HUD_PlayerInGame.cpp
@@ -1,20 +1,9 @@
 #include "UI/HUD_PlayerInGame.h"
 
 #include "Blueprint/WidgetTree.h"
-#include "Components/CanvasPanel.h"
 
 
 void UHUD_PlayerInGame::NativeConstruct()
 {
 	Super::NativeConstruct();
-
-	if (!m_canvasPanel)
-	{
-		m_canvasPanel = WidgetTree->ConstructWidget<UCanvasPanel>(UCanvasPanel::StaticClass(), TEXT("CanvasPanel"));
-
-		if (m_canvasPanel)
-		{
-			WidgetTree->RootWidget = m_canvasPanel;
-		}
-	}
 }
--- Source/LeDesinforme/Public/UI/HUD_PlayerInGame.h
+++ Source/LeDesinforme/Public/UI/HUD_PlayerInGame.h
@@ -10,9 +10,6 @@ class LEDESINFORME_API UHUD_PlayerInGame : public UUserWidget
 {
 	GENERATED_BODY()
 
-private:
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "UI", meta = (AllowPrivateAccess = "true"))
-	class UCanvasPanel* m_canvasPanel;
 	
 protected:
 	virtual void NativeConstruct() override;
